c550ba6846856a554237abe0cd5f529a7b9485d6
AK-Homberger <53138495+AK-Homberger@users.noreply.github.com>
2020-02-13 10:27:40 +0100

    Added selection of WLAN type (AP or Client) and WLAN reconnect

--- ChainCounterWLAN/ChainCounterWLAN.ino
+++ ChainCounterWLAN/ChainCounterWLAN.ino
@@ -13,17 +13,25 @@
 */
 
 // Anchor Chain Remote Control / Chain Counter with WLAN.
-// Version 0.4, 12.02.2020, AK-Homberger
+// Version 0.5, 13.02.2020, AK-Homberger
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <Preferences.h>
 
-#include "index_html.h"              // Web site information Gauge / Buttons
+#include "index_html.h"              // Web site information for Gauge / Buttons
 
 #define ENABLE_DEMO 1                // Set to 1 to enable Demo Mode with up/down counter
 #define SAFETY_STOP 2                // Defines safety stop for chain up. Stops defined number of events before reaching zero
+
+
+// Wifi: Select AP or Client
+
+#define WiFiMode_AP_STA 0            // Defines WiFi Mode 0 -> AP (with IP:192.168.4.1 and  1 -> Station (client with IP: via DHCP)
+const char *ssid = "chaincount";     // Set WLAN name
+const char *password = "kette0102";  // Set password
+
 
 WebServer server(80);                // Web Server at port 80
 Preferences preferences;             // Nonvolatile storage on ESP32 - To store ChainCounter
@@ -31,7 +39,7 @@ Preferences preferences;             // Nonvolatile storage on ESP32 - To store 
 // Chain Counter
 
 #define Chain_Calibration_Value 0.33 // Translates counter impuls to meter 0,33 m per pulse
-#define Chain_Counter_Pin 32         // counter impulse is measured as interrupt on pin 32
+#define Chain_Counter_Pin 32         // Counter impulse is measured as interrupt on GPIO pin 32
 unsigned long Last_int_time = 0;     // Time of last interrupt
 unsigned long Last_event_time = 0;   // Time of last event for engine watchdog
 int ChainCounter = 0;                // Counter for chain events
@@ -40,11 +48,11 @@ portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;  // To lock/unlock interrupt
 
 // Relay
 
-#define Chain_Up_Pin 14              // GPIO Pin 14 for Chain Up Relay
-#define Chain_Down_Pin 12            // GPIO Pin 14 for Chain Down Relay
+#define Chain_Up_Pin 14              // GPIO pin 14 for Chain Up Relay
+#define Chain_Down_Pin 12            // GPIO pin 14 for Chain Down Relay
 int UpDown = 1;                      // 1 =  Chain down / count up, -1 = Chain up / count backwards
-int OnOff = 0;                       // Relay On/Off - Off = 0, On =1
-unsigned long Watchdog_Timer = 0;    // Watchdog timer to stop relay aftre 1 second inactivity e.g. connection loss to client
+int OnOff = 0;                       // Relay On/Off - Off = 0, On = 1
+unsigned long Watchdog_Timer = 0;    // Watchdog timer to stop relay after 1 second of inactivity e.g. connection loss to client
 
 
 
@@ -53,11 +61,11 @@ unsigned long Watchdog_Timer = 0;    // Watchdog timer to stop relay aftre 1 sec
 //=======================================
 void IRAM_ATTR handleInterrupt() {
 
-  if (millis() > Last_int_time + 10) {  // Debouncing. No new events for 10 Milliseconds
+  if (millis() > Last_int_time + 10) {  // Debouncing. No new events for 10 milliseconds
 
     portENTER_CRITICAL_ISR(&mux);
 
-    ChainCounter += UpDown;             // Chain Even: Count up/down
+    ChainCounter += UpDown;             // Chain event: Count up/down
     if ( (ChainCounter == SAFETY_STOP) && (UpDown == -1) && (OnOff == 1) ) {  // Safety stop
       digitalWrite(Chain_Up_Pin, LOW );
       digitalWrite(Chain_Down_Pin, LOW );
@@ -73,16 +81,17 @@ void IRAM_ATTR handleInterrupt() {
 
 
 void setup() {
+  int wifi_retry = 0;
 
-  // Ralais output
-  pinMode(Chain_Up_Pin, OUTPUT);            // sets pin as output
-  pinMode(Chain_Down_Pin, OUTPUT);          // sets pin as output
+  // Relay output
+  pinMode(Chain_Up_Pin, OUTPUT);            // Sets pin as output
+  pinMode(Chain_Down_Pin, OUTPUT);          // Sets pin as output
   digitalWrite(Chain_Up_Pin, LOW );         // Relay off
   digitalWrite(Chain_Down_Pin, LOW );       // Relay off
 
   // Init Chain Count measure with interrupt
-  pinMode(Chain_Counter_Pin, INPUT_PULLUP); // sets pin input with pullup
-  attachInterrupt(digitalPinToInterrupt(Chain_Counter_Pin), handleInterrupt, FALLING); // attaches pin to interrupt on Falling Edge
+  pinMode(Chain_Counter_Pin, INPUT_PULLUP); // Sets pin input with pullup resistor
+  attachInterrupt(digitalPinToInterrupt(Chain_Counter_Pin), handleInterrupt, FALLING); // Attaches pin to interrupt on falling edge
 
   // Init serial
   Serial.begin(115200);
@@ -91,29 +100,51 @@ void setup() {
 
   preferences.begin("nvs", false);                  // Open nonvolatile storage (nvs)
   ChainCounter = preferences.getInt("counter", 0);  // Read last saved counter
-  LastSavedCounter = ChainCounter;                  // Initialise Last counter value
+  LastSavedCounter = ChainCounter;                  // Initialise last counter value
   preferences.end();                                // Close nvs
 
   // Init WLAN AP
-  WiFi.mode(WIFI_AP);
-  delay(100);
+  if (WiFiMode_AP_STA == 0) { 
 
-  WiFi.softAP("chaincount", "kette0102"); // AP name and password. Change to your needs
+    WiFi.mode(WIFI_AP);                              // WiFi Mode Access Point
+    delay (100);
+    WiFi.softAP(ssid, password); // AP name and password
+    Serial.println("Start WLAN AP");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.softAPIP());
 
-  Serial.println("Starte AP  Chaincounter");
-  Serial.print("IP Adresse ");
-  Serial.println(WiFi.softAPIP());
+  } else {
+
+    Serial.println("Start WLAN Client DHCP");         // WiFi Mode Client with DHCP
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {           // Check connection
+      wifi_retry++;
+      delay(500);
+      Serial.print(".");
+      if (wifi_retry >10) {
+        Serial.println("\nReboot");                   // Reboot after 10 connection tries
+        ESP.restart();
+      }  
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+  }
 
 
   // Handle HTTP request events
 
-  server.on("/", Ereignis_Index);
-  server.on("/gauge.min.js", Ereignis_js);
-  server.on("/ADC.txt", Ereignis_ChainCount);
-  server.on("/up", Ereignis_Up);
-  server.on("/down", Ereignis_Down);
-  server.on("/stop", Ereignis_Stop);
-  server.on("/reset", Ereignis_Reset);
+  server.on("/", Event_Index);
+  server.on("/gauge.min.js", Event_js);
+  server.on("/ADC.txt", Event_ChainCount);
+  server.on("/up", Event_Up);
+  server.on("/down", Event_Down);
+  server.on("/stop", Event_Stop);
+  server.on("/reset", Event_Reset);
 
   server.onNotFound(handleNotFound);
 
@@ -122,8 +153,8 @@ void setup() {
 }
 
 
-void Ereignis_Up() {                  // Handle UP request
-  Ereignis_ChainCount();              // Send response
+void Event_Up() {                  // Handle UP request
+  Event_ChainCount();              // Send response
   Serial.println("Up");
   digitalWrite(Chain_Up_Pin, HIGH );
   digitalWrite(Chain_Down_Pin, LOW );
@@ -133,8 +164,8 @@ void Ereignis_Up() {                  // Handle UP request
 }
 
 
-void Ereignis_Down() {                  // Handle UP request
-  Ereignis_ChainCount();                // Send response
+void Event_Down() {                  // Handle UP request
+  Event_ChainCount();                // Send response
   Serial.println("Down");
   digitalWrite(Chain_Up_Pin, LOW );
   digitalWrite(Chain_Down_Pin, HIGH );
@@ -143,43 +174,43 @@ void Ereignis_Down() {                  // Handle UP request
   OnOff = 1;
 }
 
-void Ereignis_Stop() {                   // Handle Stop reques
-  Ereignis_ChainCount();                 // Send response
+void Event_Stop() {                   // Handle Stop reques
+  Event_ChainCount();                 // Send response
   Serial.println("Stop");
   digitalWrite(Chain_Up_Pin, LOW );
   digitalWrite(Chain_Down_Pin, LOW );
   OnOff = 0;
 }
 
-void Ereignis_Reset() {                  // Handle reset request to reset counter to zero
+void Event_Reset() {                  // Handle reset request to reset counter to 0
   ChainCounter = 0;
-  Ereignis_ChainCount();                 // Send response
+  Event_ChainCount();                 // Send response
   Serial.println("Reset");
 }
 
 
 
-void Ereignis_Index() {   // Wenn "http://<ip address>/" aufgerufen wurde
-  server.send(200, "text/html", indexHTML);  //dann Index Webseite senden
+void Event_Index() {                         // If "http://<ip address>/" requested
+  server.send(200, "text/html", indexHTML);  // Send Index Website
 }
 
 
-void Ereignis_js() {     // Wenn "http://<ip address>/gauge.min.js" aufgerufen wurde
-  server.send(200, "text/html", gauge);     // dann gauge.min.js senden
+void Event_js() {                            // If "http://<ip address>/gauge.min.js" requested
+  server.send(200, "text/html", gauge);      // Then send gauge.min.js
 }
 
 
 
-void Ereignis_ChainCount() {    // Wenn "http://<ip address>/ADC.txt" aufgerufen wurde
+void Event_ChainCount() {                    // If  "http://<ip address>/ADC.txt" requested
 
   float temp = (ChainCounter * Chain_Calibration_Value); // Chain in meters
   server.sendHeader("Cache-Control", "no-cache");
   server.send(200, "text/plain", String (temp));
 
-  Watchdog_Timer = millis();   // Watchdog timer is set to current uptime
+  Watchdog_Timer = millis();                 // Watchdog timer is set to current uptime
 
 
-#if ENABLE_DEMO == 1                   // Demo Mode - Counts automatically UP/Down every 500 ms
+#if ENABLE_DEMO == 1                         // Demo Mode - Counts automatically UP/Down every 500 ms
 
   if (OnOff == 1) ChainCounter += UpDown;
 
@@ -193,17 +224,18 @@ void Ereignis_ChainCount() {    // Wenn "http://<ip address>/ADC.txt" aufgerufen
 
 }
 
-void handleNotFound() {
+void handleNotFound() {                       // Unknown request. Send error 404
   server.send(404, "text/plain", "File Not Found\n\n");
 }
 
 
 
 void loop() {
+  int wifi_retry = 0;
 
   server.handleClient();                                           // Handle HTTP requests
 
-  if ( ( millis() > Watchdog_Timer + 1000 ) ||                     // Check connnection
+  if ( ( millis() > Watchdog_Timer + 1000 ) ||                     // Check HTTP connnection
        ( (OnOff == 1) && (millis() > Last_event_time + 1000)) )  { // Check events if engine is on
 
     digitalWrite(Chain_Up_Pin, LOW );                              // Relay off after 1 second inactivity
@@ -216,6 +248,23 @@ void loop() {
     preferences.putInt("counter", ChainCounter);
     LastSavedCounter = ChainCounter;
     preferences.end();
+  }
+
+  if (WiFiMode_AP_STA == 1) {                                      // Check connection if working as client
+    
+    while (WiFi.status() != WL_CONNECTED && wifi_retry < 5 ) {     // Connection lost, 5 tries to reconnect
+      wifi_retry++;
+      Serial.println("WiFi not connected. Try to reconnect");
+      WiFi.disconnect();
+      WiFi.mode(WIFI_OFF);
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      delay(100);
+    }
+    if (wifi_retry >= 5) {
+      Serial.println("\nReboot");                                  // Did not work -> restart ESP32
+      ESP.restart();
+    }
   }
 
   // Dummy to empty input buffer to avoid board to stuck with e.g. NMEA Reader
